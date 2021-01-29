6e892991257374ba8408eb03ee2108c3458066d4
oscar <oscarGradoClase@gmail.com>
2021-01-29 11:43:18 +0100

    [FIX] renovado método insertarNotas

--- src/main/java/com/kike/colegio/dao/NotasDAO.java
+++ src/main/java/com/kike/colegio/dao/NotasDAO.java
@@ -8,16 +8,14 @@ import com.kike.colegio.dtos.NotasDTO;
 
 public interface NotasDAO {
 	List<Notas> obtenerAsignaturas();
-	
 	List<Notas> obtenerNotasTodo( String id_alumno,String alumnos, String id_asignatura, String notas, String fecha);
-
 	List<NotasDTO> obtenerNombreAsignaturaNota(String nombreAlumno, String nombreAsignatura, String fecha);
-	
-	Integer insertarNotas(String alumnos, String idas, String notas, String fecha);
-	 
+	//Integer insertarNotas(String alumnos, String idas, String notas, String fecha);
+	Integer insertarNota(String idAlumno, String idAsignatura,String nota, String fecha);
 	Integer actualizarNotas(String idAlumno,String idasignatura, String fecha );
-	
+	//hibernate Integer actualizarNota(String idNota, String idAlumno, String idAsignatura, String nota, String fecha);
 	Integer borrarNotas (String id);
+
 
 	
 }
